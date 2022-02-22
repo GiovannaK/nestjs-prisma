e5dbdae278692ea7a0fe2ccc957b34418adddd25
Giovanna Cunha <giovannakelli@gmail.com>
2022-02-22 00:54:46 -0300

    prisma create migration

---
+++ prisma/migrations/20220222035304_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" TEXT NOT NULL,
+    "height" INTEGER,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "pokemonId" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pokemon_name_key" ON "Pokemon"("name");
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_pokemonId_fkey" FOREIGN KEY ("pokemonId") REFERENCES "Pokemon"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
