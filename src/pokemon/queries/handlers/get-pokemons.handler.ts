6343cc1c87f68fb1f3a963509fb3a44edb69918e
Giovanna Cunha <giovannakelli@gmail.com>
2022-03-02 01:16:53 -0300

    add images relationship to list query

--- src/pokemon/queries/handlers/get-pokemons.handler.ts
+++ src/pokemon/queries/handlers/get-pokemons.handler.ts
@@ -6,7 +6,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @QueryHandler(GetPokemonsQuery)
 export class GetPokemonsHandler implements IQueryHandler<GetPokemonsQuery> {
   constructor(private readonly prisma: PrismaService) {}
+
+  private readonly _include = {
+    Images: {
+      select: {
+        url: true,
+      },
+    },
+  };
+
   async execute(query: GetPokemonsQuery): Promise<Pokemon[]> {
-    return await this.prisma.pokemon.findMany();
+    return await this.prisma.pokemon.findMany({
+      include: this._include as any,
+    });
   }
 }
